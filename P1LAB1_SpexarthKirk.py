16cb9532c5083d1ef0bccf2b286f59209168bc75
spexartk <spexartk4642@student.faytechcc.edu>
2025-06-06 17:43:59 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_SpexarthKirk.py
@@ -0,0 +1,14 @@
+ # Your Name Kirk Spexarth
+ # Date June 6, 2025
+ # Assignment Name P1LAB1
+ # A brief description of the project: This Program prompts the user to enter their first and last name seperately
+ # and then displays a welcome message. This welcome message includes the full name.
+
+print("Input first name here:")
+first_name = input()
+
+print("Input last name here:")
+last_name = input()
+
+print("Welcome to my first program in Python! For CTI-110-1902, By: ", first_name, last_name)
+ 
